8891b120ff8c22df798e8a49294ba99d2a9f1417
BothSann <boththann76@gmail.com>
2025-06-05 18:20:06 +0700

    Add styled components for App layout and UI elements
    - Implemented styled components for Button, Input, Heading, and Row.
    - Enhanced App component structure with global styles and layout.
    - Added functionality for check-in and check-out buttons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import styled from "styled-components";
+import { GlobalStyles } from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
+const StyleApp = styled.div`
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyleApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading as="h1">Wild Oasis</Heading>
+
+            <div>
+              <Heading as="h2">Check in and out</Heading>
+              <Button onClick={() => alert("Checked In!")}>Check in now</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Checked Out!")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of Guest" />
+              <Input type="number" placeholder="Number of Guest" />
+            </form>
+          </Row>
+        </Row>
+      </StyleApp>
+    </>
+  );
 }
 
 export default App;
