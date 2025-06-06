cae740d03dced5f497e0d4cc4ccbf5ce84324510
BothSann <boththann76@gmail.com>
2025-06-06 22:51:23 +0700

    Refactor ESLint configuration for improved readability and consistency

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,30 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    // Original rule
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+
+    // Common rules set to warning level
+    "no-unused-vars": "warn", // Unused variables
+    "react/prop-types": "warn", // Missing prop validations
+    "react-hooks/exhaustive-deps": "warn", // Dependencies in hooks
+    "no-debugger": "warn", // Debugger statements
+    "prefer-const": "warn", // Let that could be const
+    "no-empty": "warn", // Empty block statements
+    semi: ["warn", "always"], // Missing semicolons
+    "no-extra-semi": "warn", // Extra semicolons
   },
-}
+};
